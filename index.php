35461bacf298ee189c3c196b18588dc6cde3d534
fiknaufalh <18221096@mahasiswa.itb.ac.id>
2024-07-03 11:26:21 +0700

    feat: implement multiple servers caching

--- index.php
+++ index.php
@@ -1,15 +1,43 @@
 <?php
-  $memcached = new Memcached();
-  $memcached->addServer('memcached', 11211);
 
-  $key = 'sample_key';
-  $value = 'Hello, Memcached!';
+$memcached = new Memcached();
+$servers = [
+    ['memcached', 11211],
+    ['memcached2', 11211],
+    ['memcached3', 11211],
+    ['memcached4', 11211]
+];
+$memcached->addServers($servers);
+// $memcached->addServer('memcached', 11211);
 
-  // Set value in cache
-  $memcached->set($key, $value);
+$key = 'sample_key';
+$value = 'Hello, Memcached!';
 
-  // Get value from cache
-  $cachedValue = $memcached->get($key);
+// Set value in cache
+$memcached->set($key, $value);
 
-  echo "Cached Value: " . $cachedValue;
+// Get value from cache
+$cachedValue = $memcached->get($key);
+
+echo "Cached Value: " . $cachedValue;
+
+function run($memcached) {
+    foreach ([1, 2, 3, 4, 5, 6, 7, 8, 9] as $a) {
+        $key = "foo" . $a;
+        $value = "bar" . $a;
+        $memcached->set($key, $value, 3600);
+    }
+}
+
+run($memcached);
+
+function read($memcached) {
+    foreach ([1, 2, 3, 4, 5, 6, 7, 8, 9] as $a) {
+        $key = "foo" . $a;
+        $data = $memcached->get($key);
+        echo "<br/>" . $key . " : " . $data;
+    }
+}
+
+read($memcached);
 ?>
